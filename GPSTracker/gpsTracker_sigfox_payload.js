2acbd4bd02102ec2ba1474ce935df0c4cc91a7bb
Carles Garriga Estradé <carles.garriga@est.fib.upc.edu>
2017-08-20 13:32:14 +0200

    Updated batteryLevel and GPS coordinates

--- GPSTracker/gpsTracker_sigfox_payload.js
+++ GPSTracker/gpsTracker_sigfox_payload.js
@@ -1,12 +1,11 @@
-
-let directionsLatitudeLegend = {
+let directionsLongitudeLegend = {
   0: -1,
   1: 1,
   16: -1,
   17: 1
 };
 
-let directionsLongitudeLegend = {
+let directionsLatitudeLegend = {
   0: 1,
   1: 1,
   16: -1,
@@ -15,15 +14,15 @@ let directionsLongitudeLegend = {
 
 function main(params, callback){
     let gpsDirection = parseInt('0x' + params.data.substring(20, 22));
-    let gpsLongitude = parseInt('0x' + params.data.substring(4, 12))/ Math.pow(10, 6);
-    let gpsLatitude = parseInt('0x' + params.data.substring(12, 20))/ Math.pow(10, 6);
-    let longitude = directionsLongitudeLegend[gpsDirection] * (gpsLongitude + (gpsLongitude - Math.floor(gpsLongitude))/60.0);
-    let latitude = directionsLatitudeLegend[gpsDirection] * (gpsLatitude + (gpsLatitude - Math.floor(gpsLatitude)) / 60.0);
+    let gpsLatitude = parseInt('0x' + params.data.substring(4, 12))/ Math.pow(10, 6);
+    let gpsLongitude = parseInt('0x' + params.data.substring(12, 20))/ Math.pow(10, 6);
+    let longitude = directionsLongitudeLegend[gpsDirection] * (Math.floor(gpsLongitude) + 100.0*(gpsLongitude - Math.floor(gpsLongitude)) / 60.0);
+    let latitude = directionsLatitudeLegend[gpsDirection] * (Math.floor(gpsLatitude)+ 100.0*(gpsLatitude - Math.floor(gpsLatitude)) / 60.0);
 
     let result = [
       {
         "key": "batteryLevel",
-        "value": (0.001289 * parseInt('0x' + params.data.substring(0,2))).toFixed(2)
+        "value": (0.01289 * parseInt('0x' + params.data.substring(0,2))).toFixed(2)
       },
       {
         "key": "temperature",
