9c5905a1c95a88cd32718ae35256e20279c0eafe
Carles Garriga Estradé <carles.garriga@est.fib.upc.edu>
2017-08-22 11:54:32 +0200

    Corrected typo

--- ColdChain/sigfox_parser_coldchain.js
+++ ColdChain/sigfox_parser_coldchain.js
@@ -2,7 +2,7 @@ function main(params, callback){
 	let batteryVoltage = (parseInt(params.data.substring(2, 4), 16) * (3.4/255.0)).toFixed(2);
 	let temperature = - 40.0 + (parseInt(params.data.substring(4, 6), 16)/2.0);
 	let humidity = (parseInt(params.data.substring(10, 12), 16)/2.0);
-	let motion = (parseInt(params.data.substring(8,10),16);
+	let motion = (parseInt(params.data.substring(8,10),16));
 	    
       let result = [
         {
@@ -12,7 +12,7 @@ function main(params, callback){
         {
           key: "temperature",
           value: temperature
-        }
+        },
         {
 	  key: "humidity",
 	  value: humidity
