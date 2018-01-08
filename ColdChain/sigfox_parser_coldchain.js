fea1549db483c765483a915be8f051a15f35328a
Marc Pous <marc.pous@gmail.com>
2018-01-08 11:10:59 +0100

    Added Sigfox metadata

--- ColdChain/sigfox_parser_coldchain.js
+++ ColdChain/sigfox_parser_coldchain.js
@@ -6,7 +6,7 @@ function main(params, callback){
 	let humidity = (parseInt(params.data.substring(10, 12), 16)/2.0);
 	let motion = (parseInt(params.data.substring(8,10),16));
 	let percentage = batteryVoltage > 1.5 ? 100 : (((batteryVoltage-minimumBatteryLevel)/(1.5-minimumBatteryLevel))*100).toFixed(0)
-    percentage = percentage < 0 ? 0 : percentage;
+    	percentage = percentage < 0 ? 0 : percentage;
 	    
       let result = [
         {
@@ -26,7 +26,35 @@ function main(params, callback){
           value: motion
         },
         {
-	  key: "percentage",
+          key: "snr",
+          value: params.custom.snr
+        },
+        {
+          key: "time",
+          value: params.custom.time
+        },
+        {
+          key: "duplicate",
+          value: params.custom.duplicate
+        },
+        {
+          key: "station",
+          value: params.custom.station
+        },
+        {
+          key: "avgSnr",
+          value: params.custom.avgSnr
+        },
+        {
+          key: "rssi",
+          value: params.custom.rssi
+        },
+        {
+          key: "seqNumber",
+          value: params.custom.seqNumber
+        },
+        {
+	  key: "batteryPercentage",
 	  value: percentage
 	}
 
