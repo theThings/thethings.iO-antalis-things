a4d2b079b5375e453eb74de350e207f1ba480bf5
Marc Pous <marc.pous@gmail.com>
2018-01-29 10:11:20 +0100

    update max battery level

--- GPSTracker/gpsTracker_sigfox_payload.js
+++ GPSTracker/gpsTracker_sigfox_payload.js
@@ -16,12 +16,15 @@ function main(params, callback){
     let gpsDirection = parseInt('0x' + params.data.substring(20, 22));
     let gpsLatitude = parseInt('0x' + params.data.substring(4, 12))/ Math.pow(10, 6);
     let gpsLongitude = parseInt('0x' + params.data.substring(12, 20))/ Math.pow(10, 6);
+  
     let latitude = directionsLongitudeLegend[gpsDirection] * (Math.floor(gpsLongitude) + 100.0*(gpsLongitude - Math.floor(gpsLongitude)) / 60.0);
     let longitude = directionsLatitudeLegend[gpsDirection] * (Math.floor(gpsLatitude)+ 100.0*(gpsLatitude - Math.floor(gpsLatitude)) / 60.0);
-    let batteryVoltage = ((3.4/255.0) * parseInt('0x' + params.data.substring(0,2))).toFixed(2);
   
+    let batteryVoltage = ((3.4/255.0) * parseInt('0x' + params.data.substring(0,2))).toFixed(2);
+    
+    let maximumBatteryLevel = 1.6;
     let minimumBatteryLevel = 0.7; // In volts
-    let percentage = batteryVoltage > 1.5 ? 100 : (((batteryVoltage-minimumBatteryLevel)/(1.5-minimumBatteryLevel))*100).toFixed(0)
+    let percentage = batteryVoltage > maximumBatteryLevel ? 100 : (((batteryVoltage-minimumBatteryLevel)/(maximumBatteryLevel-minimumBatteryLevel))*100).toFixed(0)
     percentage = percentage < 0 ? 0 : percentage;
   
     let result = [
@@ -60,10 +63,6 @@ function main(params, callback){
       {
         "key": "batteryPercentage",
         "value": percentage
-      },
-      {
-        "key": "temperature",
-        "value": (- 40.0 + parseInt('0x' + params.data.substring(2,4)) / 2.0).toFixed(2)
       },
       {
         "key": "moving",
